c04a0efb8aa815331e3965ea566b4a0db880bcc5
Brendon Waters <brendonwaters@uvic.ca>
2023-09-17 21:44:13 -0700

    Break out work into functions; Parse resp into list.

--- SmartClient.py
+++ SmartClient.py
@@ -9,16 +9,26 @@ import re
 
 def main():
     URL = sys.argv[1].strip()
-    print(URL)
+    print("URL Entered: " + URL)
     match = parseURL(URL)
     HOST = match.group(0)
-    print(HOST)
+    print("Host name obtained: " + HOST)
     PORT = 80
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    data = sendRequest(HOST, PORT)
+    print("Received", repr(data))
+    data = parseResp(data)
+    print(data)
+
+def sendRequest(HOST, PORT):
+    with socket.socket(socket.AF_INET,socket.SOCK_STREAM) as s:
         s.connect((HOST, PORT))
         s.sendall(b'GET / HTTP/1.1\r\nHost: ' + bytes(HOST, 'utf-8') + b'\r\nAccept: text/html\r\n\r\n')
         data = s.recv(1024)
-    print('Received', repr(data))
+    return data
+
+def parseResp(data):
+    parsedData = data.decode().split('\r\n ')
+    return parsedData
 
 def parseURL(input):  
     pattern = re.compile(r"[A-Za-z0-9]+.*([A-Za-z0-9]+(\.[A-Za-z0-9]+)+)", re.IGNORECASE)
