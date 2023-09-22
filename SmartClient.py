d68c02c0408d9ae026c6f662857199a2a2b8bbf1
Brendon Waters <brendonwaters@uvic.ca>
2023-09-22 12:43:04 -0700

    Corrected handling for case where PROTOCOL not provided and determing HTTP(S) when given

--- SmartClient.py
+++ SmartClient.py
@@ -6,37 +6,50 @@
 import sys
 import socket
 import re
+import ssl
+
+# This is for checking HTTPS from tutorials
+# context = ssl.create_default_context()
+# conn = context.wrap_socket(socket.socket(socket.AF_INET), server_hostname="www.google.ca")
 
 def main():
     URL = sys.argv[1].strip()
     print("URL Entered: " + URL)
     match = parseURL(URL)
-    if match.group(2) != '':
-        PROTOCOL = 'HTTP'
-    else:
+    if match.group(2) is not None and not '':
         PROTOCOL = match.group(2).upper()
+    else:
+        PROTOCOL = 'HTTP'
     HOST = match.group(3)
-    print("Host name obtained: " + HOST)
+    print("Hostname obtained: " + HOST)
     print("Protocol: " + PROTOCOL)
     if match.group(4) !='':
         PORT = int(match.group(4))
     else:
         PORT = int(80)
     print("Port: " + str(PORT))
-    data = sendRequest(PROTOCOL, HOST, PORT)
-    # print("Received: ", repr(data))
-    # data = parseResp(data)
-    # print("Received: " + repr(data))
+    ENDPOINT = '/'
+    data = sendRequest(PROTOCOL, HOST, PORT, ENDPOINT)
+    print("Received: ", repr(data))
     print("-----Response Header-----")
     for key,value in data.items():
         print(key + ': '+ value)
 
-def sendRequest(PROTOCOL, HOST, PORT):
+def sendRequest(PROTOCOL, HOST, PORT, ENDPOINT):
     with socket.socket(socket.AF_INET,socket.SOCK_STREAM) as s:
         s.connect((HOST, PORT))
-        s.sendall(b'GET / ' + bytes(PROTOCOL,'utf-8') + b'/1.1\r\nHost: ' + bytes(HOST, 'utf-8') + b'\r\nConnection: Keep-Alive\r\nAccept: text/html\r\n\r\n')
+        s.sendall(b'GET ' + bytes(ENDPOINT, 'utf-8') + b' ' + bytes(PROTOCOL,'utf-8') + b'/1.1\r\nHost: ' + bytes(HOST, 'utf-8') + b'\r\nConnection: Keep-Alive\r\nAccept: text/html\r\n\r\n')
         data = parseResp(s.recv(1024))
-        # if 'HTTP/1.0' in data is '302 Moved Temporarily':    
+        print(data.get('HTTP/1.1'))
+        print(data.get('HTTP/1.0'))
+        # if (data.get('HTTP/1.1') == '302 Moved Temporarily' or '301 Moved Permanently'):
+            # print('moved 1.1')
+            # parseURL(data.get('HTTP/1.1'))
+            # ENDPOINT = data.get()
+        while not (data.get('HTTP/1.0') == '302 Moved Temporarily' or '301 Moved Permanently'):
+            print('moved 1.0')
+            match = parseURL(data.get('Location'))
+            sendRequest(PROTOCOL, HOST, PORT, ENDPOINT = match.group(3))
     return data
 
 def parseResp(data):
